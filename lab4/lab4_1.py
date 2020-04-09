c31e7d7009edc44ce8cb432275a4a136447bc379
Steven Phan <steven.phan009@gmail.com>
2020-04-09 14:03:52 -0700

    finsihed 1.2

--- lab4/lab4_1.py
+++ lab4/lab4_1.py
@@ -51,4 +51,44 @@ print("Theoretical  Measurement for Standard Deviation:",exp_meas)
 print("")
 
 #----- 1.2 Simulate an Exponentiallly distrubuted Random Variable -------------
+beta = 40
+
+# Generate the values of the RV x
+x = np.random.exponential(beta,n) 
+
+# Create bins and histogram
+nbins = 30;     # Number of bins
+edgecolor = 'w' # Color seperating bars in the bargraph 
+
+bins = [float(x) for x in np.linspace(a,b,nbins+1)]
+h1, bin_edges = np.histogram(x,bins,density=True)
+
+# Define points on the horizontal axis
+be1 = bin_edges[0:np.size(bin_edges) - 1]
+be2 = bin_edges[1:np.size(bin_edges)]
+b1 = (be1+be2)/2
+barwidth = b1[1] - b1[0] #Width of bars in the bargraph
+plt.close('all')
+
+# Plot the Bar Graph
+fig1 = plt.figure(1)
+plt.bar(b1,h1, width=barwidth, edgecolor=edgecolor)
+
+f = UnifPDF(a,b,b1)
+plt.plot(b1,f,'r')
+
+# Plot the exponential pdf
+def ExpoPDF(beta,x):
+    f = (1/beta) * np.exp((-1 / beta) * x)* np.ones(np.size(x))
+    return f
+f = ExpoPDF(beta,b1)
+plt.plot(b1,f,'r')
+plt.title('Exponential Distribution')
+plt.xlabel('Random Variable')
+plt.ylabel('Probability')
+
+plt.show()
+
+
+#----- 1.3 Simulate a Normal Random Variable -------------
 
